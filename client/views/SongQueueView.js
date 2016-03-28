0b5841a03859b38409af0df2fa5897b74bbbae22
aphinith <aphinith84@gmail.com>
2016-03-28 15:59:05 -0700

    Implement Library Entry View song click functionality

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -2,11 +2,17 @@
 var SongQueueView = Backbone.View.extend({
 
   initialize: function() {
+    //below still in progress
+    // defaults: {
+    // playFirst: function(song) {
+    //   console.log('initialize playFirst!');
+    // }
+  // },
   },
 
   render: function() {
-  initialize: function() {
-  },
+    initialize: function() {
+    };
   }
 
 });
